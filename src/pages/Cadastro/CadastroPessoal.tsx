a91af452bee93bcbca41e5802ffe02bbd261f00d
Astaphaanos <Projeto Finalizado>
2024-12-17 09:56:21 -0300

    Lidando com erros e validações

--- src/pages/Cadastro/CadastroPessoal.tsx
+++ src/pages/Cadastro/CadastroPessoal.tsx
@@ -1,42 +1,90 @@
 import { Button, Label, Fieldset, Input, Form, Titulo } from "../../components";
 import { useForm } from "react-hook-form";
+import { ErrorMessage } from "../../components";
 
-// Definir os dados que serão recebidos no formulario
+//? Definir os dados que serão recebidos no formulario
 interface FormInputTipos {
   nome: string;
   email: string;
+  telefone: string;
   senha: string;
   senhaVerificada: string;
 }
 
 const CadastroPessoal = () => {
-  const { register,handleSubmit } = useForm<FormInputTipos>(); // tipando o Hook e dizendo que ele vai receber dados desse tipo
+  const { 
+    register,
+    handleSubmit, 
+    formState: {errors}, //* função do useform de errors
+    watch, //* assistir a senha que é colocada no input para usar no validaSenha (função)
+    } 
+    = useForm<FormInputTipos>(); //* tipando o Hook (useForm<FormInputTipos>) e dizendo que ele vai receber dados desse tipo
 
   const aoSubmeter = (dados: FormInputTipos) => {
     console.log(dados);
   }
 
+  //? Para assistir a senha que é colocada no campo senha para usar na validação e confirmação da senha
+  const senha = watch ('senha')
+
+  //? Criar um objeto para senhaVerificada
+  const validaSenha = {
+    obrigatorio: (val: string) => !!val || "Por favor, insira a senha novamente", //! vai receber um valor (val: string), converter o val em boleano (!!val) e se ela não for (||) válida vai ter a mensagem de erro
+    tamanhoMinimo: (val: string) => val.length >= 8 || "A senha deve ter pelo menos 6 caracteres", //! verificar se o tamanho do val é maior ou igual a 8 
+    senhaIguais: (val: string) => val == senha || "As senhas não correspondem" //! verificar se o val é igual a senha passada
+  }
+
+  //? Criar validações personalizadas
+  function validarEmail(valor: string){
+    const formatoEmail = /^[^\s@]+@alura\.com\.br$/;
+    if (!formatoEmail.test(valor)) {
+      console.error("Endereço de email é inválido para este domínio");
+      return false;
+    }
+    return true;
+}
+
   return (
     <>
       <Titulo>Insira alguns dados básicos:</Titulo>
+
       <Form onSubmit={handleSubmit(aoSubmeter)}>  
+
         <Fieldset>
           <Label htmlFor="campo-nome">Nome</Label>
           <Input
             id="campo-nome"
             placeholder="Digite seu nome completo"
             type="text"
-            {...register('nome')} // espalhando(split) o register, ele tem que receber um nome que tem que ser diferente para cada um
+            //? Passar a borda vermelha no formulário quando invalido
+            $error={!!errors.nome} //! props convertido em boleado (!!)
+            {...register('nome', {
+              required: "Campo de nome é obrigatório", //* mensagem que vai passar caso o campo seja inválido
+              minLength: {
+                value: 5,
+                message: "O nome deve ter pelo menos 5 caracteres"
+              }
+            })} 
+            //! O register recebe o nome do campo e um objeto, onde passamos nossa validação (baseado no HTML nativo)
+            //* espalhando(...split) o register, ele tem que receber um nome que tem que ser diferente para cada um
           />
+          {errors.nome && <ErrorMessage>{errors.nome.message}</ErrorMessage>} {/* Usando o errors do UseForm */}
         </Fieldset>
+
         <Fieldset>
           <Label htmlFor="campo-email">E-mail</Label>
           <Input
             id="campo-email"
             placeholder="Insira seu endereço de email"
             type="email"
-            {...register('email')}
+            {...register('email', {
+              required: "O campo de email é obrigatório", //* mensagem que vai passar caso o campo seja inválido
+              validate: validarEmail //* validação personalizada
+              //* Como o validate realmente funciona (lá em cima foi uma forma abrevidada): validate: (val) => validarEmail(val)
+              //! Pode passar várias funções de validação
+            })}
           />
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>} 
         </Fieldset>
 
         <Fieldset>
@@ -44,9 +92,17 @@ const CadastroPessoal = () => {
           <Input
             id="campo-telefone"
             type="text"
-            placeholder="Ex: (DDD) XXXXX-XXXX"
-            {...register('telefone')}
+            placeholder="Ex: (DD) XXXXX-XXXX"
+            {...register("telefone", {
+              pattern: {
+                value: /^\(\d{2,3}\) \d{5}-\d{4}$/,
+                message: "O telefone inserido está no formato incorreto" 
+              }, 
+              required: "O campo telefone é obrigatório", 
+            })}
+            //* pattern faz com que a gente só aceite telefones desse tipo (igual ao placeholder)
           />
+          {errors.telefone && <ErrorMessage>{errors.telefone.message}</ErrorMessage>}
         </Fieldset>
 
         <Fieldset>
@@ -55,8 +111,16 @@ const CadastroPessoal = () => {
             id="campo-senha"
             placeholder="Crie uma senha"
             type="password"
-            {...register('senha')}
+            {...register('senha',{
+              required: "O campo de senha é obrigatório",
+              minLength: {
+                value: 8,
+                message: "A senha deve ter pelo menos 8 caracteres"
+              }
+            })}
           />
+          {errors.senha && <ErrorMessage>{errors.senha.message}</ErrorMessage>} 
+
         </Fieldset>
         <Fieldset>
           <Label htmlFor="campo-senha-confirmacao">Repita a senha</Label>
@@ -64,8 +128,12 @@ const CadastroPessoal = () => {
             id="campo-senha-confirmacao"
             placeholder="Repita a senha anterior"
             type="password"
-            {...register('senha-verificada')}
+            {...register('senhaVerificada', {
+              required: 'Repita a senha',
+              validate: validaSenha,
+            })}
           />
+          {errors.senhaVerificada && <ErrorMessage>{errors.senhaVerificada.message}</ErrorMessage>} 
         </Fieldset>
         <Button type="submit">Avançar</Button>
       </Form>
