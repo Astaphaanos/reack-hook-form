b79987dd137d877a384d7df66e52132d22755663
Astaphaanos <Projeto Finalizado>
2024-12-18 09:39:52 -0300

    Validação do CadastroEndereco, API de CEP e Utilização do mask para formatação do campo de telefone

--- src/pages/Cadastro/CadastroEndereco.tsx
+++ src/pages/Cadastro/CadastroEndereco.tsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import {
   Button,
   Fieldset,
@@ -7,39 +8,145 @@ import {
   Label,
   Titulo,
 } from "../../components";
+import { ErrorMessage } from "../../components";
+
+//! Vai validar do mesmo jeito do CadastroPessoal
+
+interface FormInputEndereco {
+  cep: string;
+  rua: string;
+  numero: string;
+  bairro: string;
+  localidade: string;
+}
 
 const CadastroEndereco = () => {
+
+  const { register, handleSubmit, setError, setValue, watch, formState: {errors}} = useForm<FormInputEndereco>({
+      mode: 'all', //* revalide os campos quando o foco sair dele para tirar os erros quando o usuario preencher o cep correto
+      defaultValues: { //* Os campos tem o valor inicial "" (começa vazio)
+        cep: '',
+        rua: '',
+        bairro: '',
+        numero: '',
+        localidade: ''
+      }
+  });
+  
+  const aoSubmeter = (dados: FormInputEndereco) => {
+    console.log(dados);
+  }
+
+  //* Assistir o campo cep
+  const cepDigitado = watch('cep');
+
+  //* Função que vai retornar a API de CEP
+    const fethEndereco = async (cep: string) => {
+      if (!cep) {
+        setError("cep", { //! setError é do useForm para fazer error customizados
+          type: "manual", 
+          message: "Cep Inválido"
+        });
+        return
+      }
+
+      try {
+        const response = await fetch(`http://viacep.com.br/ws/${cep}/json/`)
+        const data = await response.json();
+
+        //* Preenchimento automático dos campos se o cep for válido
+        if(response.ok) {
+          setValue('rua', data.logradouro);
+          setValue('localidade', `${data.localidade}, ${data.uf}`); //* Dessa forma consigo pegar dos valores (localidade e uf)
+          setValue('bairro', data.bairro);
+        } else {
+          throw new Error("Cep inválido") //* Passar o error
+        }
+
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+
   return (
     <>
       <Titulo>Agora, mais alguns dados sobre você:</Titulo>
-      <Form>
+
+      <Form onSubmit={handleSubmit(aoSubmeter)}>
         <Fieldset>
           <Label htmlFor="campo-cep">CEP</Label>
-          <Input id="campo-cep" placeholder="Insira seu CEP" type="text" />
+          <Input 
+          id="campo-cep" 
+          placeholder="Insira seu CEP" 
+          type="text"
+          {...register("cep",{
+            required: "O campo é obrigatório"
+          })}
+          $error={!!errors.cep}
+          onBlur={() => fethEndereco(cepDigitado)}     
+          />
+          {errors.cep && <ErrorMessage>{errors.cep.message}</ErrorMessage>}
         </Fieldset>
+
         <Fieldset>
           <Label htmlFor="campo-rua">Rua</Label>
-          <Input id="campo-rua" placeholder="Rua Agarikov" type="text" />
+          <Input 
+          id="campo-rua" 
+          placeholder="Rua Agarikov" 
+          type="text" 
+          $error={!!errors.rua}
+          {...register("rua", {
+            required: "O campo é obrigatório"
+          })}
+          />
+          {errors.rua && <ErrorMessage>{errors.rua.message}</ErrorMessage>}
         </Fieldset>
 
         <FormContainer>
           <Fieldset>
             <Label htmlFor="campo-numero-rua">Número</Label>
-            <Input id="campo-numero-rua" placeholder="Ex: 1440" type="text" />
+            <Input 
+            id="campo-numero-rua" 
+            placeholder="Ex: 1440" 
+            type="text" 
+            $error={!!errors.numero}
+            {...register("numero",{
+              required: "O campo é obrigatório"
+            })}
+            />
+            {errors.numero && <ErrorMessage>{errors.numero.message}</ErrorMessage>}
           </Fieldset>
+
           <Fieldset>
             <Label htmlFor="campo-bairro">Bairro</Label>
-            <Input id="campo-bairro" placeholder="Vila Mariana" type="text" />
+            <Input 
+            id="campo-bairro" 
+            placeholder="Vila Mariana" 
+            type="text" 
+            $error={!!errors.bairro}
+            {...register("bairro", {
+              required: "O campo é obrigatório"
+            })}
+            />
+            {errors.bairro && <ErrorMessage>{errors.bairro.message}</ErrorMessage>}
           </Fieldset>
         </FormContainer>
+
         <Fieldset>
           <Label htmlFor="campo-localidade">Localidade</Label>
           <Input
             id="campo-localidade"
             placeholder="São Paulo, SP"
             type="text"
+            $error={!!errors.localidade}
+            {...register("localidade", {
+              required: "O campo é obrigatório"
+            })}
           />
+          {errors.localidade && <ErrorMessage>{errors.localidade.message}</ErrorMessage>}
         </Fieldset>
+
         <Button type="submit">Cadastrar</Button>
       </Form>
     </>
--- src/pages/Cadastro/CadastroPessoal.tsx
+++ src/pages/Cadastro/CadastroPessoal.tsx
@@ -1,6 +1,10 @@
 import { Button, Label, Fieldset, Input, Form, Titulo } from "../../components";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
+//! Controller: usa-se para trabalhar com componentes que são controlados (componentes que já estão prontos de bibliotecas 
+// !externas) diferente do que o react-hook-from prefere
 import { ErrorMessage } from "../../components";
+import InputMask from "../../components/InputMask";
+import { useEffect } from "react";
 
 //? Definir os dados que serão recebidos no formulario
 interface FormInputTipos {
@@ -12,43 +16,61 @@ interface FormInputTipos {
 }
 
 const CadastroPessoal = () => {
-  const { 
+  const {
     register,
-    handleSubmit, 
-    formState: {errors}, //* função do useform de errors
+    handleSubmit,
+    formState: { errors, isSubmitSuccessful}, //* função do useform de errors
     watch, //* assistir a senha que é colocada no input para usar no validaSenha (função)
-    } 
-    = useForm<FormInputTipos>(); //* tipando o Hook (useForm<FormInputTipos>) e dizendo que ele vai receber dados desse tipo
+    control,
+    reset,
+  }
+    = useForm<FormInputTipos>( {
+      mode: 'all',
+      defaultValues: {
+        nome: '', 
+        email: '', 
+        telefone: '', 
+        senha: '', 
+        senhaVerificada: '', 
+      },
+    }); //* tipando o Hook (useForm<FormInputTipos>) e dizendo que ele vai receber dados desse tipo
+
+    //! Pedindo para o useEffect ficar de olho nessa variavel (isSubmitSuccessful), para quando a variavel 
+    //! mudar ele renderize a aplicação limpandos os dados porque estou passando o reset
+    useEffect(() => {
+      reset();
+    }, [reset, isSubmitSuccessful]);
+
 
   const aoSubmeter = (dados: FormInputTipos) => {
     console.log(dados);
   }
 
   //? Para assistir a senha que é colocada no campo senha para usar na validação e confirmação da senha
-  const senha = watch ('senha')
+  const senha = watch('senha')
 
   //? Criar um objeto para senhaVerificada
   const validaSenha = {
     obrigatorio: (val: string) => !!val || "Por favor, insira a senha novamente", //! vai receber um valor (val: string), converter o val em boleano (!!val) e se ela não for (||) válida vai ter a mensagem de erro
-    tamanhoMinimo: (val: string) => val.length >= 8 || "A senha deve ter pelo menos 6 caracteres", //! verificar se o tamanho do val é maior ou igual a 8 
+    tamanhoMinimo: (val: string) => val.length >= 8 || "A senha deve ter pelo menos 8 caracteres", //! verificar se o tamanho do val é maior ou igual a 8 
     senhaIguais: (val: string) => val == senha || "As senhas não correspondem" //! verificar se o val é igual a senha passada
   }
 
   //? Criar validações personalizadas
-  function validarEmail(valor: string){
+  function validarEmail(valor: string) {
     const formatoEmail = /^[^\s@]+@alura\.com\.br$/;
     if (!formatoEmail.test(valor)) {
       console.error("Endereço de email é inválido para este domínio");
       return false;
     }
     return true;
-}
+  }
 
   return (
     <>
       <Titulo>Insira alguns dados básicos:</Titulo>
 
-      <Form onSubmit={handleSubmit(aoSubmeter)}>  
+      <Form onSubmit={handleSubmit(aoSubmeter)}>
 
         <Fieldset>
           <Label htmlFor="campo-nome">Nome</Label>
@@ -57,18 +79,19 @@ const CadastroPessoal = () => {
             placeholder="Digite seu nome completo"
             type="text"
             //? Passar a borda vermelha no formulário quando invalido
-            $error={!!errors.nome} //! props convertido em boleado (!!)
-            {...register('nome', {
-              required: "Campo de nome é obrigatório", //* mensagem que vai passar caso o campo seja inválido
-              minLength: {
-                value: 5,
-                message: "O nome deve ter pelo menos 5 caracteres"
-              }
-            })} 
-            //! O register recebe o nome do campo e um objeto, onde passamos nossa validação (baseado no HTML nativo)
-            //* espalhando(...split) o register, ele tem que receber um nome que tem que ser diferente para cada um
+            $error={!!errors.nome} //! props convertido em boleado (!!), aparece a bordinha vermelha no input
+            {...register('nome', //* espalhando(...split) o register, ele tem que receber um nome que tem que ser diferente para cada um
+              {
+                required: "Campo de nome é obrigatório", //* mensagem que vai passar caso o campo seja inválido
+                minLength: {
+                  value: 5,
+                  message: "O nome deve ter pelo menos 5 caracteres"
+                }
+              })}
+          //! O register recebe o nome do campo e um objeto, onde passamos nossa validação (baseado no HTML nativo)
+
           />
-          {errors.nome && <ErrorMessage>{errors.nome.message}</ErrorMessage>} {/* Usando o errors do UseForm */}
+          {errors.nome && <ErrorMessage>{errors.nome.message}</ErrorMessage>}{/* Usando o errors do UseForm, vai aparecer a mensagem de erro vermelha em baixo do input*/}
         </Fieldset>
 
         <Fieldset>
@@ -77,6 +100,7 @@ const CadastroPessoal = () => {
             id="campo-email"
             placeholder="Insira seu endereço de email"
             type="email"
+            $error={!!errors.email}
             {...register('email', {
               required: "O campo de email é obrigatório", //* mensagem que vai passar caso o campo seja inválido
               validate: validarEmail //* validação personalizada
@@ -84,26 +108,34 @@ const CadastroPessoal = () => {
               //! Pode passar várias funções de validação
             })}
           />
-          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>} 
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
         </Fieldset>
 
-        <Fieldset>
-          <Label>Telefone</Label>
-          <Input
-            id="campo-telefone"
-            type="text"
-            placeholder="Ex: (DD) XXXXX-XXXX"
-            {...register("telefone", {
-              pattern: {
-                value: /^\(\d{2,3}\) \d{5}-\d{4}$/,
-                message: "O telefone inserido está no formato incorreto" 
-              }, 
-              required: "O campo telefone é obrigatório", 
-            })}
-            //* pattern faz com que a gente só aceite telefones desse tipo (igual ao placeholder)
-          />
-          {errors.telefone && <ErrorMessage>{errors.telefone.message}</ErrorMessage>}
-        </Fieldset>
+        //! COntroller para o telefone
+        <Controller 
+        control={control} 
+        name="telefone" 
+        rules={{
+          pattern: {
+            value: /^\(\d{2, 3}\) \d{5}-\d{4}$/,
+            message: "O telefone inserido está no formato incorreto",
+            },
+            required: "O campo telefone é obrigatório",
+          }}
+          //! Reponsável por renderizar o componente controlado
+          render={({field}) => ( //* deestruturar um objeto chamado field e renderizar o componente mask dentro dos ()
+            <Fieldset>
+              <Label>Telefone</Label>
+              <InputMask 
+              mask= "(99) 99999-9999"
+              placeholder="Ex: (DD) XXXXX-XXXX"
+              $error={!!errors.telefone}
+              onChange={field.onChange} //! Serve para controlar o componente controlado (trabalha com componentes de bibliotecas externas)
+              />
+              {errors.telefone && <ErrorMessage>{errors.telefone.message}</ErrorMessage>}
+            </Fieldset>
+          )}
+        />
 
         <Fieldset>
           <Label htmlFor="campo-senha">Crie uma senha</Label>
@@ -111,7 +143,8 @@ const CadastroPessoal = () => {
             id="campo-senha"
             placeholder="Crie uma senha"
             type="password"
-            {...register('senha',{
+            $error={!!errors.senha}
+            {...register('senha', {
               required: "O campo de senha é obrigatório",
               minLength: {
                 value: 8,
@@ -119,7 +152,7 @@ const CadastroPessoal = () => {
               }
             })}
           />
-          {errors.senha && <ErrorMessage>{errors.senha.message}</ErrorMessage>} 
+          {errors.senha && <ErrorMessage>{errors.senha.message}</ErrorMessage>}
 
         </Fieldset>
         <Fieldset>
@@ -128,12 +161,13 @@ const CadastroPessoal = () => {
             id="campo-senha-confirmacao"
             placeholder="Repita a senha anterior"
             type="password"
+            $error={!!errors.senhaVerificada}
             {...register('senhaVerificada', {
               required: 'Repita a senha',
               validate: validaSenha,
             })}
           />
-          {errors.senhaVerificada && <ErrorMessage>{errors.senhaVerificada.message}</ErrorMessage>} 
+          {errors.senhaVerificada && <ErrorMessage>{errors.senhaVerificada.message}</ErrorMessage>}
         </Fieldset>
         <Button type="submit">Avançar</Button>
       </Form>
